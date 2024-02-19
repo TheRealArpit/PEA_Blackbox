144c44dc0c76f5f65d8dd4340c74539c4735998c
TheRealArpit <mirdhaa67@gmail.com>
2024-02-19 15:55:48 +0000

    nothing

--- APE_Blackbox/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ape_blackbox {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.ape_blackbox to javafx.fxml;
-    exports com.example.ape_blackbox;
-}
