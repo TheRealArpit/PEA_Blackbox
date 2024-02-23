e9845c13d54355370a11af4d58dac94744b1067d
TheRealArpit <mirdhaa67@gmail.com>
2024-02-23 14:27:12 +0000

    Deleted a project that we weren't using and renamed test2 to blackbox

--- blackbox/src/MainApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.blackbox;
-
-public class MainApplication {
-    public static void main(String[] args) {
-        //BlackBoxGame g = new BlackBoxGame();
-        //BlackBoxGame.launch(BlackBoxGame.class, args);
-    }
-
-}
