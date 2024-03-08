2c69fd3073e79e0e519cedf4e803dcd563d04618
Elvis-E <126807238+Elvis-E@users.noreply.github.com>
2024-03-08 15:46:42 +0000

    Update Ray.java
    Made a feature for the bottomleft, bottomright, and the right hexagon bordering an atom. that lets the ray, hit, and reflect

--- BlackBoxx/src/main/java/Ray.java
+++ BlackBoxx/src/main/java/Ray.java
@@ -82,7 +82,56 @@ public class Ray {
                         goingTo = ConstantValues.direction.S_WEST;
                         createRay();
                     }
+                }else if (hextocheck.atomPlacement == ConstantValues.atomPlacement.DOWNLEFT) {
+                    if (goingTo == ConstantValues.direction.N_EAST) {
+                        createLine();
+                        System.out.println("hit");
+                        break;
+                    }
+                    else if (goingTo == ConstantValues.direction.EAST) {
+                        createLine();
+                        goingTo = ConstantValues.direction.S_EAST;
+                        createRay();
+                    }
+                    else if (goingTo == ConstantValues.direction.N_WEST) {
+                        createLine();
+                        goingTo = ConstantValues.direction.WEST;
+                        createRay();
+                    }
+                } else if (hextocheck.atomPlacement == ConstantValues.atomPlacement.DOWNRIGHT) {
+                    if (goingTo == ConstantValues.direction.N_WEST) {
+                        createLine();
+                        System.out.println("hit");
+                        break;
+                    }
+                    else if (goingTo == ConstantValues.direction.WEST) {
+                        createLine();
+                        goingTo = ConstantValues.direction.S_WEST;
+                        createRay();
+                    }
+                    else if (goingTo == ConstantValues.direction.N_EAST) {
+                        createLine();
+                        goingTo = ConstantValues.direction.EAST;
+                        createRay();
+                    }
+                }else if (hextocheck.atomPlacement == ConstantValues.atomPlacement.RIGHT) {
+                    if (goingTo == ConstantValues.direction.WEST) {
+                        createLine();
+                        System.out.println("hit");
+                        break;
+                    }
+                    else if (goingTo == ConstantValues.direction.S_WEST) {
+                        createLine();
+                        goingTo = ConstantValues.direction.S_EAST;
+                        createRay();
+                    }
+                    else if (goingTo == ConstantValues.direction.N_WEST) {
+                        createLine();
+                        goingTo = ConstantValues.direction.N_EAST;
+                        createRay();
+                    }
                 }
+
 
 
                 }else{
