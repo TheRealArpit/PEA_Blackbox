b040001a66de715bcf9355d6f63c6070ce335b8a
TheRealArpit <mirdhaa67@gmail.com>
2024-04-10 11:48:53 +0100

    test werent added to git

---
+++ BlackBoxx/src2/BlackboxTest/HexBoardTest.java
@@ -0,0 +1,206 @@
+package BlackboxTest;
+import Blackbox.Constant.Constants;
+import Blackbox.Model.*;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+
+import  static Blackbox.Constant.Constants.*;
+import static org.junit.Assert.assertEquals;
+
+import javafx.scene.layout.Pane;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+
+public class HexBoardTest {
+
+    @Test
+    public void createHexagonalBoard() {
+        TESTING = true;
+        String coor =
+                "(0, 0)\t(0, 1)\t(0, 2)\t(0, 3)\t(0, 4)\t\n" +
+                        "(1, 0)\t(1, 1)\t(1, 2)\t(1, 3)\t(1, 4)\t(1, 5)\t\n" +
+                        "(2, 0)\t(2, 1)\t(2, 2)\t(2, 3)\t(2, 4)\t(2, 5)\t(2, 6)\t\n" +
+                        "(3, 0)\t(3, 1)\t(3, 2)\t(3, 3)\t(3, 4)\t(3, 5)\t(3, 6)\t(3, 7)\t\n" +
+                        "(4, 0)\t(4, 1)\t(4, 2)\t(4, 3)\t(4, 4)\t(4, 5)\t(4, 6)\t(4, 7)\t(4, 8)\t\n" +
+                        "(5, 0)\t(5, 1)\t(5, 2)\t(5, 3)\t(5, 4)\t(5, 5)\t(5, 6)\t(5, 7)\t\n" +
+                        "(6, 0)\t(6, 1)\t(6, 2)\t(6, 3)\t(6, 4)\t(6, 5)\t(6, 6)\t\n" +
+                        "(7, 0)\t(7, 1)\t(7, 2)\t(7, 3)\t(7, 4)\t(7, 5)\t\n" +
+                        "(8, 0)\t(8, 1)\t(8, 2)\t(8, 3)\t(8, 4)\t\n";
+        StringBuilder add = new StringBuilder();
+
+        HexBoard hexBoard = new HexBoard();
+        hexBoard.createHexagonalBoard();
+        for (int i = 0; i < hexBoard.getHexList().size(); i++) {
+            List<Hexagon> curr = hexBoard.getHexList().get(i);
+            for (int j = 0; j < curr.size(); j++) {
+                add.append("(" + i + ", " + j + ")\t");
+            }
+            add.append("\n");
+        }
+        assertEquals(coor, add.toString());
+
+    }
+    @Test
+    public void createatomsOnBoard() {
+        TESTING = true;
+        Pane display = new Pane();
+        HexBoard hexBoard = new HexBoard();
+        hexBoard.createHexagonalBoard();
+        hexBoard.createAtomAthexagon(0,0);
+        hexBoard.createAtomAthexagon(3,4);
+        hexBoard.createAtomAthexagon(4,8);
+        assertEquals(true, hexBoard.getHexagon(0,0).hasAtom());
+        assertEquals(true, hexBoard.getHexagon(3,4).hasAtom());
+        assertEquals(true, hexBoard.getHexagon(4,8).hasAtom());
+
+        assertEquals(true, hexBoard.getHexagon(3,4).hasAtom());
+        assertEquals(true, hexBoard.getHexagon(4,8).hasAtom());
+
+    }
+    @Test
+    public void deleteatomsOnBoard(){
+        TESTING = true;
+        Pane display = new Pane();
+        HexBoard hexBoard = new HexBoard();
+        hexBoard.createHexagonalBoard();
+        hexBoard.createAtomAthexagon(0,0);
+        hexBoard.deleteAtomAthexagon(0,0);
+        assertEquals(false, hexBoard.getHexagon(0,0).hasAtom());
+        hexBoard.createAtomAthexagon(1,5);
+        hexBoard.deleteAtomAthexagon(1,5);
+        assertEquals(false, hexBoard.getHexagon(1,5).hasAtom());
+    }
+    @Test
+    public void SixtyDegreeReflection() {
+        TESTING = true;
+        Pane display = new Pane();
+        HexBoard hexBoard = new HexBoard();
+        hexBoard.createHexagonalBoard();
+        hexBoard.createAtomAthexagon(4,4);
+        hexBoard.initializeHexagonsNearAtom();
+
+        //testing all the posibilites ray hits the middle atom on the board
+        assertEquals("17->3",hexBoard.sendRayat(17));
+        assertEquals("19->Hit",hexBoard.sendRayat(19));        //19 is direct hit. Hits the hexagon on the circle of influence. buttom left of 4,4
+        assertEquals("21->35",hexBoard.sendRayat(21));
+
+        assertEquals("28->Hit",hexBoard.sendRayat(28));
+        assertEquals("26->12",hexBoard.sendRayat(26));
+        assertEquals("30->44",hexBoard.sendRayat(30));
+
+        assertEquals("48->8",hexBoard.sendRayat(48));
+        assertEquals("46->Hit",hexBoard.sendRayat(46));
+        assertEquals("44->30",hexBoard.sendRayat(44));
+
+        assertEquals("3->17",hexBoard.sendRayat(3));
+        assertEquals("1->Hit",hexBoard.sendRayat(1));
+        assertEquals("53->39",hexBoard.sendRayat(53));
+
+        assertEquals("39->53",hexBoard.sendRayat(39));
+        assertEquals("37->Hit",hexBoard.sendRayat(37));
+        assertEquals("35->21",hexBoard.sendRayat(35));
+
+        assertEquals("8->48",hexBoard.sendRayat(8));
+        assertEquals("10->Hit",hexBoard.sendRayat(10));
+        assertEquals("12->26",hexBoard.sendRayat(12));
+    }
+    @Test
+    public void OneTwentyDegreeReflection() {
+        TESTING = true;
+        Pane display = new Pane();
+        HexBoard hexBoard = new HexBoard();
+        hexBoard.createHexagonalBoard();
+        hexBoard.createAtomAthexagon(4, 4);
+        hexBoard.createAtomAthexagon(4, 5);
+        hexBoard.createAtomAthexagon(4, 3);
+        hexBoard.createAtomAthexagon(4, 2);
+        hexBoard.initializeHexagonsNearAtom();
+        //testing all some possibilities ray hits between two atoms (causing a 120 degree reflection)
+        assertEquals("28->21", hexBoard.sendRayat(28));
+        assertEquals("26->19", hexBoard.sendRayat(26));
+        assertEquals("24->17", hexBoard.sendRayat(24));
+
+        assertEquals("53->46", hexBoard.sendRayat(53));
+        assertEquals("1->48", hexBoard.sendRayat(1));
+        assertEquals("50->3", hexBoard.sendRayat(50));
+    }
+    @Test
+    public void EquallyDistantReflection() {
+        TESTING = true;
+        Pane display = new Pane();
+        HexBoard hexBoard = new HexBoard();
+        hexBoard.createHexagonalBoard();
+        hexBoard.createAtomAthexagon(2, 1);
+        hexBoard.createAtomAthexagon(4, 2);
+        hexBoard.createAtomAthexagon(6, 1);
+        hexBoard.createAtomAthexagon(7, 2);
+        hexBoard.createAtomAthexagon(7, 3);
+        hexBoard.createAtomAthexagon(6, 5);
+
+
+        hexBoard.initializeHexagonsNearAtom();
+
+        //testing all the possibilities ray hits equally distant atoms (causing a 180 degree reflection)
+        assertEquals("8->8", hexBoard.sendRayat(8));
+        assertEquals("39->39", hexBoard.sendRayat(39));
+
+        assertEquals("12->12", hexBoard.sendRayat(12));
+
+        assertEquals("19->19", hexBoard.sendRayat(19));
+        assertEquals("46->46", hexBoard.sendRayat(46));
+
+        assertEquals("28->28", hexBoard.sendRayat(28));
+
+    }
+    @Test
+    public void complexPath() {
+        TESTING = true;
+        HexBoard hexBoard = new HexBoard();
+        hexBoard.createHexagonalBoard();
+
+        hexBoard.createAtomAthexagon(3, 1);
+        hexBoard.createAtomAthexagon(6, 2);
+        hexBoard.createAtomAthexagon(6, 5);
+        hexBoard.createAtomAthexagon(3, 7);
+        hexBoard.initializeHexagonsNearAtom();
+
+        assertEquals("50->49", hexBoard.sendRayat(50));
+
+        HexBoard HexBoard1 = new HexBoard();
+        HexBoard1.createHexagonalBoard();
+        HexBoard1.createAtomAthexagon(1, 3);
+        HexBoard1.createAtomAthexagon(4, 6);
+        HexBoard1.createAtomAthexagon(5, 5);
+        HexBoard1.initializeHexagonsNearAtom();
+
+        assertEquals("6->10", HexBoard1.sendRayat(6));
+
+        HexBoard HexBoard_ = new HexBoard();
+        HexBoard_.createHexagonalBoard();
+        HexBoard_.initializeHexagonsNearAtom();
+
+        HexBoard_.createAtomAthexagon(7, 0);
+        HexBoard_.createAtomAthexagon(5, 4);
+        HexBoard_.createAtomAthexagon(4, 2);
+        HexBoard_.createAtomAthexagon(2, 4);
+        HexBoard_.initializeHexagonsNearAtom();
+        assertEquals("22->53", HexBoard_.sendRayat(22));
+    }
+    @Test
+    public void internalReflection() {
+        TESTING = true;
+        HexBoard HexBoard = new HexBoard();
+        HexBoard.createHexagonalBoard();
+        HexBoard.createAtomAthexagon(4, 8);
+        HexBoard.createAtomAthexagon(7, 5);
+        HexBoard.createAtomAthexagon(8, 1);
+        HexBoard.initializeHexagonsNearAtom();
+        assertEquals("39->39", HexBoard.sendRayat(39));
+        assertEquals("29->29", HexBoard.sendRayat(29));
+        assertEquals("23->23", HexBoard.sendRayat(23));
+    }
+
+}
