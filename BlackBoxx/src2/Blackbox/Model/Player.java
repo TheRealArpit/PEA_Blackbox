810c74a97e3ffa4ece9a1f6ddce091eaaac1caf2
TheRealArpit <mirdhaa67@gmail.com>
2024-04-16 13:40:09 +0100

    created Player class

--- BlackBoxx/src2/Blackbox/Model/Player.java
+++ BlackBoxx/src2/Blackbox/Model/Player.java
@@ -1,11 +1,80 @@
 package Blackbox.Model;
+import Blackbox.Model.HexBoard;
+
+import java.util.ArrayList;
 
 public class Player {
-    private int id;
-    private int score;
+    private int Totalscore;
 
-    public Player(int id){
-        this.id = id;
+    private int numOfRays;
+    private int numOfGuesses;
+    private int numofCorrectGuesses;
+    private int numofWrongGuesses;
+    private int numOfMarkers;
+    private ArrayList<String> history;
+
+    public Player() {
+        this.numOfRays=0;
+        this.numOfMarkers = 0;
+        this.numOfRays = 0;
+        this.numOfGuesses = 0;
+        this.numofCorrectGuesses = 0;
+        this.numofWrongGuesses = 0;
+        this.history = new ArrayList<String>();
     }
+
+    //--------Getters
+    public int getNumOfGuesses() {
+        return numOfGuesses;
+    }
+
+    public int getNumofCorrectGuesses() {
+        return numofCorrectGuesses;
+    }
+
+    public int getNumofWrongGuesses() {
+        return numofWrongGuesses;
+    }
+    public int getNumofMarkers() {
+        return numOfMarkers;
+    }
+    public int getScore() {
+        return Totalscore;
+    }
+    public ArrayList<String> getHistory() {
+        return history;
+    }
+
+
+    //--------Setters / adders
+    public void setNumOfWrongGuesses(int i){
+        numofWrongGuesses = i;
+    }
+    public void addNumOfRays() {
+        this.numOfRays++;
+    }
+    public void addNumMarkers(int i) {
+        this.numOfMarkers+= i;
+    }
+    public void setHistory(ArrayList<String> history){
+        this.history = history;
+    }
+    public void addNumOfGuesses() {
+        numOfGuesses++;
+    }
+
+    public void addNumofCorrectGuesses() {
+        numofCorrectGuesses++;
+    }
+    public void subNumofCorrectGuesses() {
+        numofCorrectGuesses--;
+    }
+    public int calculateScore() {
+        Totalscore += numOfMarkers + 5*numofWrongGuesses;
+        return Totalscore;
+    }
+
+
+
 
 }
