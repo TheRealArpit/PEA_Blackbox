d70139ba2c8d7c784e1e9e7ba1d3e8137f78ec4b
TheRealArpit <mirdhaa67@gmail.com>
2024-04-16 13:39:33 +0100

    deleted src1

--- BlackBoxx/src/Blackbox/Launcher.java
+++
@@ -1,7 +0,0 @@
-package Blackbox;
-
-public class Launcher {
-    public static void main(final String[] args) {
-        Main.launch(Main.class,args);
-    }
-}
--- BlackBoxx/src/GUIStarter.java
+++
@@ -1,8 +0,0 @@
-import Blackbox.Main;
-
-public class GUIStarter {
-
-    public static void main(final String[] args) {
-        Main.main(args);
-    }
-}
