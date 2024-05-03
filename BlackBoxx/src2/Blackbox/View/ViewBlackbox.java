3bf61267d2364145e1fda2e641e9a0ea233f603e
Elvis-E <126807238+Elvis-E@users.noreply.github.com>
2024-05-03 17:27:24 +0100

    Edited the ending game screen - Faces change based on score

--- BlackBoxx/src2/Blackbox/View/ViewBlackbox.java
+++ BlackBoxx/src2/Blackbox/View/ViewBlackbox.java
@@ -4,15 +4,23 @@ import Blackbox.Model.HexBoard;
 import Blackbox.Model.Player;
 import Blackbox.Model.WelcomeScreen;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
+
+import static Blackbox.Constant.Constants.WELCOMEBOARD_X_STARTAT;
 
 public class ViewBlackbox extends Application {
     private Scene hexBoardScene;
@@ -96,46 +104,132 @@ public class ViewBlackbox extends Application {
     private void displayScore(Pane display) {
         display.getChildren().clear();  // Clear the board
 
+        //Setting background display appearance
+        display.setStyle("-fx-background-color: linear-gradient(#000000 , #001B4F);");
+
+        //Player faces for end screen
+        String face1 = "(:";
+        String face2 = ":)";
+
         // Determine the winner based on the lower score
         String winnerText;
         if (player1.getScore() < player2.getScore()) {
             winnerText = "Winner: Player 1";
+            face2 = ":(";
         } else if (player2.getScore() < player1.getScore()) {
             winnerText = "Winner: Player 2";
+            face1 = "):";
         } else {
             winnerText = "It's a tie!";
         }
 
-        // Display the score and the winner
-        String scoreText = "Score - Player 1: " + player1.getScore() + ", Player 2: " + player2.getScore() + "\n" + winnerText;
+        String scoreText = "Score\nPlayer 1: " + player1.getScore() + "\t\t\t\t\t\tPlayer 2: " + player2.getScore() + "\n\n" + winnerText;
         Text scoreDisplay = new Text(scoreText);
-        scoreDisplay.setFill(Color.RED);
-        scoreDisplay.setFont(new Font(20)); // Increase text size
+        scoreDisplay.setFill(Color.WHITE);
+        scoreDisplay.setFont(Font.font("Impact", FontWeight.BOLD, 70)); // Increase text size
+        scoreDisplay.setTextAlignment(TextAlignment.CENTER);
+
+        // Create a drop shadow effect for outline
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setRadius(10);
+        dropShadow.setColor(Color.RED);
+        scoreDisplay.setEffect(dropShadow);
 
         // Center the score display on the display pane
-        scoreDisplay.setX((display.getWidth() - scoreDisplay.getLayoutBounds().getWidth()) / 2);
-        scoreDisplay.setY(50);
+        scoreDisplay.setX(((display.getWidth() - scoreDisplay.getLayoutBounds().getWidth()) / 2 ) + 10);
+        scoreDisplay.setY(100);
+
 
         // Restart button
         Button restartButton = new Button("Restart Game");
-        restartButton.setLayoutX(10);
-        restartButton.setLayoutY(100);
         restartButton.setOnAction(event -> startGame(display));  // Set the action to restart the game
 
+        restartButton.setLayoutX(200 + WELCOMEBOARD_X_STARTAT); // Set the x-coordinate of the button
+        restartButton.setLayoutY(450); // Set the y-coordinate of the button
+        restartButton.setAlignment(Pos.CENTER);
+
+        //Setting button Font
+        restartButton.setFont(Font.font("Impact", FontWeight.BOLD, 36));
+
+        //Changing style of Button
+        restartButton.setStyle("-fx-background-color: linear-gradient(#4ECCFC, #09729A );"
+                +  "-fx-background-radius: 90;"
+                +  "-fx-background-insets: 0;"
+                +  "-fx-text-fill: white;"
+                +  "-fx-min-width: 200px;"
+                +  "-fx-min-height: 100px;");
+
+
         // Exit button
-        Button exitButton = new Button("Exit");
-        exitButton.setLayoutX(10);
-        exitButton.setLayoutY(150);
+        Button exitButton = new Button("Exit Game");
+        exitButton.setLayoutX(217+WELCOMEBOARD_X_STARTAT); // Set the x-coordinate of the button
+        exitButton.setLayoutY(610); // Set the y-coordinate of the button
+        exitButton.setAlignment(Pos.CENTER);
+
+        //Setting button Font
+        exitButton.setFont(Font.font("Impact", FontWeight.BOLD, 36));
+
+        //Changing style of Button
+        exitButton.setStyle("-fx-background-color: linear-gradient(#4ECCFC, #09729A );"
+                +  "-fx-background-radius: 90;"
+                +  "-fx-background-insets: 0;"
+                +  "-fx-text-fill: white;"
+                +  "-fx-min-width: 210px;"
+                +  "-fx-min-height: 100px;");
+
         exitButton.setOnAction(event -> {
             // Close the window or the application
             Stage stage = (Stage) display.getScene().getWindow();
             stage.close();
         });
 
+
+        Button player1Face = new Button(face1);
+        player1Face.setStyle("-fx-background-color: linear-gradient(#F155F6, #FA1536 );"
+                +  "-fx-background-radius: 90;"
+                +  "-fx-background-insets: 0;"
+                +  "-fx-text-fill: white;"
+                +  "-fx-min-width: 210px;"
+                +  "-fx-min-height: 100px;");
+        player1Face.setFont(Font.font("Impact", FontWeight.BOLD, 100)); // Increase text size
+
+        // Center the face on the display pane
+        player1Face.setLayoutX(-180+WELCOMEBOARD_X_STARTAT);
+        player1Face.setLayoutY(420);
+
+
+        //Setting rotation for face text 1
+        Rotate rotate1 = new Rotate(180);
+        player1Face.getTransforms().add(rotate1);
+
+        Button player2Face = new Button(face2);
+        player2Face.setStyle("-fx-background-color: linear-gradient(#F155F6, #FA1536 );"
+                +  "-fx-background-radius: 90;"
+                +  "-fx-background-insets: 0;"
+                +  "-fx-text-fill: white;"
+                +  "-fx-min-width: 210px;"
+                +  "-fx-min-height: 100px;");
+        player2Face.setFont(Font.font("Impact", FontWeight.BOLD, 100)); // Increase text size
+
+        // Center the face on the display pane
+        player2Face.setLayoutX(825+WELCOMEBOARD_X_STARTAT);
+        player2Face.setLayoutY(210);
+
+        //Setting rotation for face text 2
+        Rotate rotate2 = new Rotate(90);
+        player1Face.getTransforms().add(rotate2);
+
+        player2Face.getTransforms().add(rotate2);
+
+
+
+
         // Add the buttons and the text to the display
         display.getChildren().add(restartButton);
         display.getChildren().add(exitButton);
         display.getChildren().add(scoreDisplay);  // Display scores and winner
+        display.getChildren().add(player1Face);
+        display.getChildren().add(player2Face);
     }
 
     public void setDisplay(Pane display){
